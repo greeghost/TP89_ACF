5920d321f2e9069cf781ea7fcf854e4611b2e55b
greeghost <hugo.boulier@ens-rennes.fr>
2021-11-17 14:43:39 +0100

    tri message

--- tp89.thy
+++ tp89.thy
@@ -16,6 +16,20 @@ datatype message=
 
 type_synonym transaction= "transid * nat"
 
+fun id :: "message \<Rightarrow> transid" where
+"id (Cancel i) = i" |
+"id (Pay i _) = i" |
+"id (Ack i _) = i"
+
+
+fun add :: "message \<Rightarrow> message list list \<Rightarrow> message list list" where
+"add x [] = [[x]]" |
+"add x ([] # yss) = add x yss" |
+"add x ((y # ys) # yss) = (if (id x = id y) then ((x # (y # ys)) # yss) else ((y # ys) # (add x yss)))"
+
+fun tri_message :: "message list \<Rightarrow> message list list" where
+"tri_message (x # xs) = add x (tri_message xs)" |
+"tri_message [] = []"
 
 
 (* ----- Exportation en Scala (Isabelle 2018) -------*)
